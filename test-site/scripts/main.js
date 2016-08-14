02ee3f43a4767002d304129a798597e986a29f96
Dylan Sherwood <dylansherwood@gmail.com>
2016-08-13 22:10:45 -0400

    first commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/maisie_bed.jpg') {
+		myImage.setAttribute('src', 'images/maisie_bed_hi.jpg');
+	} else {
+		myImage.setAttribute('src', 'images/maisie_bed.jpg');
+	}
+}
+
+
+var myButton = document.querySelector('button');
+var myPhrase = document.querySelector('#user');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myPhrase.textContent = 'Cuddling with ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myPhrase.textContent = 'Cuddling with ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
