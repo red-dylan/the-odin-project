e7e59d7b2077e36af18a4f99153c3e1594e96374
red-dylan <dylansherwood@gmail.com>
2017-07-09 08:20:54 -0400

    add stop drawing on mousedown

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -1,15 +1,30 @@
+var gridDimension = 480;
+var gridPixels = 16;
+var isDraw = true;
+
 $(document).ready(function() {
-	for (i=0; i<16; i++) {
+	for (i=0; i<gridPixels; i++) {
 		console.log("i=" + i);
 		$("#grid").append("<div id='grid-row" + i +"' class='row'></div>");
-		for (j=0; j<16; j++) {
+		for (j=0; j<gridPixels; j++) {
 			console.log("j=" + j);
 			$("#grid-row" + i).append("<div class='grid-square'></div>");
 		}
 	}
 
+	$(".grid-square").mousedown(function(e) {
+		e.preventDefault();
+		isDraw = false;
+	})
+
+	$(".grid-square").mouseup(function() {
+		isDraw = true;
+	})
+
 	$(".grid-square").hover(function() {
-		$(this).addClass("etched");
+		if (isDraw === true) {
+			$(this).addClass("etched");
+		}
 	})
 
 	$("#clear").click(function() {
